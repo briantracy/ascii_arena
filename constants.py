8874b09a8196ce0986b7512c86d8ee5cefe5ba86
Brian Tracy <brian.tracy33@gmail.com>
2019-10-29 23:34:01 -0400

    restructured file hierarchy

---
+++ constants.py
@@ -0,0 +1,17 @@
+# constants.py
+
+
+class Developer:
+    version = '0.1.0'
+
+class Internal:
+    max_username_length = 16
+    max_map_width  = 80
+    max_map_height = 40
+
+class Game:
+    # Attack cooldown in ms
+    lance_delay = 3000
+
+class Network:
+    server_port = 2718 * 2 # 5436
--- shared/constants.py
+++
@@ -1,9 +0,0 @@
-# constants.py
-
-
-class Internal:
-    max_username_length = 16
-
-class Game:
-    # Attack cooldown in ms
-    lance_delay = 3000
